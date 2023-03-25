7ddbe786f305c4cb385a5146b073413f8a347e25
ivanlado <ivanladoc01@gmail.com>
2023-03-26 00:25:24 +0100

    Fichero .l avanzado

--- lex.yy.h
+++ lex.yy.h
@@ -467,7 +467,7 @@ extern int yylex (void);
 #undef yyTABLES_NAME
 #endif
 
-#line 20 "lex.l"
+#line 56 "lex.l"
 
 
 #line 474 "lex.yy.h"
--- regression.d
+++ regression.d
@@ -1,4 +1,28 @@
 import std.stdio;
 import std.algorithm.iteration;
 import std.exception;
+"hola"
+'alsdasklda'
+//asjlkdajsdlkasd
+/*lhadskdjasd
+sdasdasds
+*/
+0
+15
+1_5
+1___5
++
+0b10_01
+//lsdkjalskd
+6546456
+e+00015
+e+_00015
+e+00015_
+e+_00015
+//laksdjaskd
+5e15
+5e15
+e+45
+E-98
+
 
